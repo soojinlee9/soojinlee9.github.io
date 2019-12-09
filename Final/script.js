b9853c6914b4d641ef9096ad02f3137090a34e8b
Soo Jin Lee <sjl705@nyu.edu>
2019-12-09 11:58:59 -0500

    Finished

--- Final/script.js
+++ Final/script.js
@@ -1,10 +1,22 @@
 var grid = document.querySelector('.grid');
-var msnry = new Masonry( grid, {
+var msnry = new Masonry( grid, { //creates masonry object to access
   itemSelector: '.grid-item',
-  fitWidth: true
+  fitWidth: true //for centering masonry
 });
 
-imagesLoaded( grid ).on( 'progress', function() {
-  // layout Masonry after each image loads
+imagesLoaded( grid ).on( 'progress', function() { // layout Masonry after each image loads (helps to not make images overlap as they are loading)
   msnry.layout();
+});
+
+
+$('[data-fancybox="images"]').fancybox({ //to add clickable link to yelp inside caption
+  caption : function( instance, item ) {
+    var caption = $(this).data('caption') || '';
+
+    if ( item.type === 'image' ) {
+      caption = (caption.length ? caption + '<br />' : '') + '<a target="_blank" href="' + 'https://www.yelp.com/' + '"><i class="fa fa-yelp"></i> Yelp</a>' ;
+    }
+
+    return caption;
+  }
 });
