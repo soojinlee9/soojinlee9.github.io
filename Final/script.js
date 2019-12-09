35bd13117135541bb76f02e058dc4b7680499f52
Soo Jin Lee <sjl705@nyu.edu>
2019-12-09 11:08:40 -0500

    Finished

--- Final/script.js
+++ Final/script.js
@@ -3,3 +3,8 @@ var msnry = new Masonry( grid, {
   itemSelector: '.grid-item',
   fitWidth: true
 });
+
+imagesLoaded( grid ).on( 'progress', function() {
+  // layout Masonry after each image loads
+  msnry.layout();
+});
