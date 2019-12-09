ca0d55c8372fcd66a63893de3319b54b47b1bccf
Soo Jin Lee <sjl705@nyu.edu>
2019-12-09 12:10:53 -0500

    Finished

--- Final/script.js
+++ Final/script.js
@@ -9,14 +9,14 @@ imagesLoaded( grid ).on( 'progress', function() { // layout Masonry after each i
 });
 
 
-// $('[data-fancybox="images"]').fancybox({ //to add clickable link to yelp inside caption
-//   caption : function( instance, item ) {
-//     var caption = $(this).data('caption') || '';
-//
-//     if ( item.type === 'image' ) {
-//       caption = (caption.length ? caption + '<br />' : '') + '<a target="_blank" href="' + 'https://www.yelp.com/' + '"><i class="fa fa-yelp"></i> Yelp</a>' ;
-//     }
-//
-//     return caption;
-//   }
-// });
+$('[data-fancybox="images"]').fancybox({ //to add clickable link to yelp inside caption
+  caption : function( instance, item ) {
+    var caption = $(this).data('caption') || '';
+
+    if ( item.type === 'image' ) {
+      caption = (caption.length ? caption + '<br />' : '') + '<a target="_blank" href="' + 'https://www.yelp.com/' + '"><i class="fa fa-yelp"></i> Yelp</a>' ;
+    }
+
+    return caption;
+  }
+});
