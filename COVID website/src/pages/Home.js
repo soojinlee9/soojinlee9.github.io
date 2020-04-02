1b5a86162869d2a3883537ce8d730de55fa25363
Soo Jin Lee <sjl705@nyu.edu>
2020-04-01 21:38:10 -0400

    update

--- COVID website/src/pages/Home.js
+++ COVID website/src/pages/Home.js
@@ -32,25 +32,25 @@ function Home() {
                     <div className="cases">
                         <h3>Total Number of Cases in {country}</h3>
                         {/* code below: initially empty so use && to get data after array is loaded */}
-                        <h1 className="numCase">{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].TotalConfirmed} </h1> 
+                        <h1 className="numCase">{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].TotalConfirmed} </h1> 
                         <h3>Number of Cases Confirmed Today</h3>
-                        <h1 className="numTodayCase">{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].NewConfirmed} </h1>
+                        <h1 className="numTodayCase">{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].NewConfirmed} </h1>
                     </div>
                 </a>
                 <a href="/deaths">
                     <div className="deaths">
                         <h3>Total Number of Deaths in {country}</h3>
-                        <h1 className="numDeaths">{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].TotalDeaths} </h1>
+                        <h1 className="numDeaths">{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].TotalDeaths} </h1>
                         <h3>Number of Deaths Today</h3>
-                        <h1 className="numTodayDeaths">{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].NewDeaths} </h1>
+                        <h1 className="numTodayDeaths">{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].NewDeaths} </h1>
                     </div>
                 </a>
                 <div className="recovered" style={{ backgroundColor: `rgba(54, 214, 70,${greenFill})` }}> 
                 {/* green background gets brighter as total number recovered gets closer to total number confirmed */}
                     <h3>Total Number of Recovered in {country}</h3>
-                    <h1>{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].TotalRecovered} </h1>
+                    <h1>{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].TotalRecovered} </h1>
                     <h3>Number of Recovered Today</h3>
-                    <h1>{countryData.Countries && countryData.Countries[217] && countryData.Countries[217].NewRecovered} </h1>
+                    <h1>{countryData.Countries && countryData.Countries[219] && countryData.Countries[219].NewRecovered} </h1>
                 </div>
             </div>
             <div className="chart">
