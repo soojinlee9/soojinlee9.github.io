3e664f81787f00c0e4a51cdf849ae9be2cfc8d71
Soo Jin Lee <sjl705@nyu.edu>
2020-04-04 15:50:02 -0400

    update

--- COVID website/src/pages/Home.js
+++ COVID website/src/pages/Home.js
@@ -6,7 +6,8 @@ import Chart from '../components/Chart';
 function Home() {
     const [countryData, setCountryData] = useState([]);
     const country = 'US';
-  
+    const [countries, setCountries] = useState(``);
+
     useEffect(() => {
         // make request to get api data and store in countryData
         axios.get(`https://api.covid19api.com/summary`)
@@ -20,13 +21,22 @@ function Home() {
             });        
     }, [country]); //once country is loaded
 
-    const numOfCountries = countryData.Countries&&countryData.Countries.length;
-    const numForUS = numOfCountries - 9; //api increases in length every day, and the US data array is always 9 less than the array length
-   
+    useEffect(() => {
+        if(countryData.Countries) { //once countryData is loaded
+            setCountries(countryData.Countries); //creates array of all country data
+
+        }
+    }, [countryData]);
+
+
+function isUS(data) {  //function to find US data
+    return data.CountryCode === 'US';
+  }
+const usData = countries&&countries.find(isUS); //making sure countries is loaded
 
     //used to style the color of recovered div background
-    const totalCases = countryData.Countries&&countryData.Countries[numForUS].TotalConfirmed; //number value of total confirmed
-    const totalRecovered = countryData.Countries&&countryData.Countries[numForUS].TotalRecovered; //number value of total recovered
+    const totalCases = usData.TotalConfirmed; //number value of total confirmed
+    const totalRecovered = usData.TotalRecovered; //number value of total recovered
     const greenFill = (totalRecovered / totalCases * 15).toFixed(2); //rounds to the nearest hundredth
 
     return (
@@ -35,26 +45,25 @@ function Home() {
                 <a href="/cases">
                     <div className="cases">
                         <h3>Total Number of Cases in {country}</h3>
-                        {/* code below: initially empty so use && to get data after array is loaded */}
-                        <h1 className="numCase">{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].TotalConfirmed} </h1> 
+                        <h1 className="numCase">{usData.TotalConfirmed} </h1> 
                         <h3>Number of Cases Confirmed Today</h3>
-                        <h1 className="numTodayCase">{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].NewConfirmed} </h1>
+                        <h1 className="numTodayCase">{usData.NewConfirmed} </h1>
                     </div>
                 </a>
                 <a href="/deaths">
                     <div className="deaths">
                         <h3>Total Number of Deaths in {country}</h3>
-                        <h1 className="numDeaths">{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].TotalDeaths} </h1>
+                        <h1 className="numDeaths">{usData.TotalDeaths} </h1>
                         <h3>Number of Deaths Today</h3>
-                        <h1 className="numTodayDeaths">{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].NewDeaths} </h1>
+                        <h1 className="numTodayDeaths">{usData.NewDeaths} </h1>
                     </div>
                 </a>
                 <div className="recovered" style={{ backgroundColor: `rgba(54, 214, 70,${greenFill})` }}> 
                 {/* green background gets brighter as total number recovered gets closer to total number confirmed */}
                     <h3>Total Number of Recovered in {country}</h3>
-                    <h1>{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].TotalRecovered} </h1>
+                    <h1>{usData.TotalRecovered} </h1>
                     <h3>Number of Recovered Today</h3>
-                    <h1>{countryData.Countries && countryData.Countries[numForUS] && countryData.Countries[numForUS].NewRecovered} </h1>
+                    <h1>{usData.NewRecovered} </h1>
                 </div>
             </div>
             <div className="chart">
